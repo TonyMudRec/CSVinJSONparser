a8108effc0b53fc5426774387314aa4f11eaba30
TonyMudRec <sm1le88man@gmail.com>
2022-06-20 14:45:12 +0300

    data.json created

--- src/main/java/CSVApp.java
+++ src/main/java/CSVApp.java
@@ -5,8 +5,6 @@ import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -14,11 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class CSVApp {
-
-    static String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-    static String fileName = "data.csv";
-    static List<Employee> list = parseCSV(columnMapping, fileName);
-    static String json = listToJson(list);
+    static List<Employee> list;
 
     public static void main(String[] args) {
 //        "1,John,Smith,USA,25"
@@ -29,7 +23,12 @@ public class CSVApp {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
         writeString(json);
+
     }
 
     private static List<Employee> parseCSV(String[] layout, String fileName) {
@@ -49,22 +48,15 @@ public class CSVApp {
     }
 
     private static String listToJson(List<Employee> list) {
-//        Type listType = new TypeToken<List<T>>() { (я так и не понял, зачем это)
-//        }.getType();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
+        //        Type listType = new TypeToken<List<T>>() {}.getType(); /* я так и не понял, зачем это */
         return gson.toJson(list, Employee.class);
     }
 
     private static void writeString(String json) {
-        JSONObject obj = new JSONObject();
-        obj.put("id", (Employee) id); /* как передать сюда значение конструктора из Employee*/
-        obj.put("firstName", json.id);
-        obj.put("lastName", json.id);
-        obj.put("country", json.id);
-        obj.put("age", json.id);
         try (FileWriter file = new FileWriter("data.json")) {
-            file.write(obj.toJSONString());
+            file.write(json);
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();
