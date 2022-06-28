fb2ba1f4bfd722844fc750c7345a846f5384df0c
TonyMudRec <sm1le88man@gmail.com>
2022-06-28 13:32:40 +0300

    exercise 1 cjmplited, some quations in exercise 2

--- src/main/java/CSVApp.java
+++ src/main/java/CSVApp.java
@@ -1,14 +1,27 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class CSVApp {
@@ -16,10 +29,10 @@ public class CSVApp {
 
     public static void main(String[] args) {
 //        "1,John,Smith,USA,25"
-//        "2,Inav,Petrov,RU,23"
-        String[] employee = "2,Inav,Petrov,RU,23".split(",");
+//        "2,Ivan,Petrov,RU,23"
+        String[] employees = "2,Ivan,Petrov,RU,23".split(",");
         try (CSVWriter csvwriter = new CSVWriter(new FileWriter("data.csv", true))) {
-            csvwriter.writeNext(employee);
+            csvwriter.writeNext(employees);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -28,6 +41,38 @@ public class CSVApp {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            Element staff = document.createElement("staff");
+            document.appendChild(staff);
+            Element employee = document.createElement("employee");
+            employee.setAttribute("id", "1");
+            employee.setAttribute("firstName", "John");
+            employee.setAttribute("lastName", "Smith");
+            employee.setAttribute("country", "USA");
+            employee.setAttribute("age", "25");
+            staff.appendChild(employee);
+            employee.setAttribute("id", "2");
+            employee.setAttribute("firstName", "Ivan");
+            employee.setAttribute("lastName", "Petrov");
+            employee.setAttribute("country", "RU");
+            employee.setAttribute("age", "23");
+            staff.appendChild(employee);
+
+            DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File("data.xml"));
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.transform(domSource, streamResult);
+
+        } catch (ParserConfigurationException | TransformerException ex) {
+            ex.printStackTrace();
+        }
+
 
     }
 
@@ -50,8 +95,9 @@ public class CSVApp {
     private static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        //        Type listType = new TypeToken<List<T>>() {}.getType(); /* я так и не понял, зачем это */
-        return gson.toJson(list, Employee.class);
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
     }
 
     private static void writeString(String json) {
